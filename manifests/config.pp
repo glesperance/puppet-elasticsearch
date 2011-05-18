e8bce593a252e63a07dcfd0d4238b8d48b29fbe0
Atha Kouroussis <akouroussis@gmail.com>
2011-05-18 14:42:29 -0400

    Fix another typo

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,7 +5,7 @@ class elasticsearch::config {
 	file { [ $elasticsearch::params::config_dir, $elasticsearch::params::log_dir, $elasticsearch::params::data_dir, $elasticsearch::params::work_dir]:
 		ensure  => directory,
 		owner   => 'root',
-		group   => 'root'
+		group   => 'root',
 		mode    => '0755',
 		notify  => Class['elasticsearch::service'],
 		require => Class['elasticsearch::install'],
